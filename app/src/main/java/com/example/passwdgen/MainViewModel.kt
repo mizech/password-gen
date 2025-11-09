ecdd7844d689bd9ac7c645a7034efe624b9bba75
mizech <michael.zech@hotmail.de>
2025-11-09 11:36:12 +0100

    Added: Comment

--- app/src/main/java/com/example/passwdgen/MainViewModel.kt
+++ app/src/main/java/com/example/passwdgen/MainViewModel.kt
@@ -14,7 +14,8 @@ class MainViewModel(): ViewModel() {
     private val _passwd
         = MutableStateFlow<String>("0")
     val passwd: StateFlow<String> = _passwd.asStateFlow()
-
+    // Todo: Add a copy-to-clipboard button.
+    // Todo: Improve the algorithm (Each group of char HAS to be included, in case of selected).
     fun generatePasswd(length: Int, options: List<Option>) {
         var feasibleChars = mutableListOf<Char>()
         var result = ""
@@ -22,24 +23,20 @@ class MainViewModel(): ViewModel() {
         options.forEach {
             if (it.title == Option.UP.title && it.isActive) {
                 feasibleChars.addAll(('A'..'Z').toList())
-                println("Contains UP.")
             }
 
             if (it.title == Option.LOW.title && it.isActive) {
                 feasibleChars.addAll(('a'..'z').toList())
-                println("Contains LOW.")
             }
 
             if (it.title == Option.NUM.title && it.isActive) {
                 feasibleChars.addAll(('0'..'9').toList())
-                println("Contains NUM.")
             }
 
             if (it.title == Option.SPEC.title && it.isActive) {
                 val specs = listOf<Char>('!', '"', '§', '$', '%', '&', '/', '(', ')', '=',
                     '?', '+', '*', '#', '-', '_', ',', ';', '<', '>')
                 feasibleChars.addAll(specs)
-                println("Contains SPEC.")
             }
         }
 
